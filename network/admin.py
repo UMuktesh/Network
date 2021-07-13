a253edb31670e3cb072be25365deb486c0a736c4
UMuktesh <u19cs080@coed.svnit.ac.in>
2021-07-13 13:13:17 +0530

    delete all

--- network/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-# from django.contrib.auth.models import AbstractUser
-from .models import User, Post
-
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Post)
-# admin.site.register(AbstractUser)
--- network/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Post
-
-class post(forms.Form):
-    description = forms.CharField(widget=forms.Textarea, label="New Post")
-    class Meta:
-        model = Post
-        fields = ("description",)
